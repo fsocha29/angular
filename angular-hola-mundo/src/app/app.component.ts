ec5b059f8258dbb6ab054cd030116c4344d03e0c
fsocha29 <mfernandos29@gmail.com>
2025-02-05 11:02:51 -0500

    cambio pruebas

---
+++ angular-hola-mundo/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SaludadorComponent } from "./saludador/saludador.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, SaludadorComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'angular-hola-mundo';
+}
---
+++ angular-hola-mundo/src/app/saludador/saludador.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saludador',
+  imports: [],
+  templateUrl: './saludador.component.html',
+  styleUrl: './saludador.component.css'
+})
+export class SaludadorComponent {
+
+}
